2211bc46e4fda9daeb128488f14c2214f56ff88b
Dung <tlbgtn@gmail.com>
2024-08-11 16:09:12 +0700

    110824: add leetcode problems

---
+++ leetcode/sql/607-Sales-Person.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT s.name FROM SalesPerson s 
+WHERE s.name NOT IN (
+    SELECT s.name from SalesPerson s LEFT JOIN Orders o ON s.sales_id = o.sales_id LEFT JOIN Company c ON o.com_id = c.com_id WHERE c.name = 'RED'
+);
