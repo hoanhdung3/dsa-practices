602d3bb2f05550b8f8294a6772bc3d25583df6d6
Ho Anh Dung <dungha@mail.wgentech.com>
2024-09-03 18:48:31 +0700

    030924: add leetcode problem

---
+++ leetcode/problem-solving/1945-Sum-of-Digits-of-String-After-Convert.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var getLucky = function(s, k) {
+    let numStr = "";
+    for(let i=0; i<s.length; i++){
+        numStr += (s.charCodeAt(i) - 96).toString();
+    }
+    while(k > 0){
+        let sum = 0;
+        for(let i=0; i<numStr.length; i++){
+            sum += Number(numStr[i]);
+        }
+        numStr = sum.toString();
+        if(sum < 10) break;
+        k--;
+    }
+    return numStr;
+};
